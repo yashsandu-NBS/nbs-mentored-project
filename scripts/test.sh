31aa97742e896ff406bd79cc31db5e8e696e946e
yashsandu-nw <71331116+yashsandu-nw@users.noreply.github.com>
2020-10-20 16:22:17 +0100

    Update test.sh

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,13 +1,19 @@
 #! /bin/bash
 
+python3 -m venv venv
+. ./venv/bin/activate
+pip3 install -r requirements.txt
+pytest
+rm -rf venv
+
 # Starting up the container
 #sudo docker run -d -p 5000:5000 --name service-1 localhost:8082/service-1
 
 # Going to the directory here app.py is located
-/home/student/nbs-mentored-project/service-1/
+#/home/student/nbs-mentored-project/service-1/
 
 # Running the test
-python -m pytest
+#python -m pytest
 
 # Deleting the container, ready for next time
 # sudo docker rm -f service-1
