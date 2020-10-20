7f18a3af061cb2abaa7359bde9671f3cfadf423d
yashsandu-nw <71331116+yashsandu-nw@users.noreply.github.com>
2020-10-20 16:26:47 +0100

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,10 +1,10 @@
 #! /bin/bash
 
 # Full path to the requirements.txt
-cd /home/student/nbs-mentored-project/
+#cd /home/student/nbs-mentored-project/
 
 # Installing the requirements.txt file
-pip install -r requirements
+#pip install -r requirements
 
 # Building the image
 sudo docker build -t localhost:8082/service-1 .
