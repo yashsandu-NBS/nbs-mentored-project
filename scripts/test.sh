5abd182c7088e4acdd72b61b8588a8d17409ff22
yashsandu-nw <71331116+yashsandu-nw@users.noreply.github.com>
2020-10-20 11:13:04 +0100

    Update test.sh

--- scripts/test.sh
+++ scripts/test.sh
@@ -7,7 +7,7 @@ sudo docker run -d -p 5000:5000 --name service-1 localhost:8082/service-1
 /home/student/nbs-mentored-project/service-1/
 
 # Running the test
-py.test
+pytest
 
 # Deleting the container, ready for next time
 sudo docker rm -f service-1
