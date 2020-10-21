78ffe2a58f56f2866fffb1830b9ce1181a7be4eb
yashsandu-nw <71331116+yashsandu-nw@users.noreply.github.com>
2020-10-21 15:13:07 +0100

    Update push.sh

--- scripts/push.sh
+++ scripts/push.sh
@@ -8,3 +8,6 @@ sudo docker exec nexus cat /nexus.data/admin.password
 
 # Push the images
 sudo docker push localhost:8082/service-1
+
+# Deleting container 
+sudo docker rm -f $(sudo docker ps -aq)
