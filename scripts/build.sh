48fe29c0510969e37020d0a7769441c79c854b88
yashsandu-nw <71331116+yashsandu-nw@users.noreply.github.com>
2020-10-22 14:49:35 +0100

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -7,4 +7,5 @@ cd /home/student/nbs-mentored-project/service-1/
 #pip install -r requirements
 
 # Building the image
-sudo docker build -t localhost:8082/service-1 .
+# sudo docker build -t localhost:8082/service-1 .
+sudo docker compose build
