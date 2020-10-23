9d59b51a3066af7c6f45c6d2e656755d8f1ede34
yashsandu-nw <71331116+yashsandu-nw@users.noreply.github.com>
2020-10-23 12:01:29 +0100

    Update push.sh

--- scripts/push.sh
+++ scripts/push.sh
@@ -4,7 +4,7 @@
 sudo docker run -d -p 8081:8081 -p 8082:8082 --name nexus sonatype/nexus3
 
 # Requesting admin password for Nexus
-sudo docker exec nexus cat /nexus.data/admin.password
+sudo docker exec nexus cat /nexus-data/admin.password
 
 # Push the images
 #sudo docker push localhost:8082/service-1
