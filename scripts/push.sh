90b57f38ae9158eb5be7a625107c8fea6217bdf7
yashsandu-nw <71331116+yashsandu-nw@users.noreply.github.com>
2020-10-23 16:18:41 +0100

    Update push.sh

--- scripts/push.sh
+++ scripts/push.sh
@@ -8,12 +8,7 @@
 
 # Push the images
 #sudo docker push localhost:8082/service-1
-#sudo docker-compose push
-
-sudo docker-compose push localhost:8082/service-1
-sudo docker-compose push localhost:8082/service-2
-sudo docker-compose push localhost:8082/service-3
-sudo docker-compose push localhost:8082/service-4
+sudo docker-compose push
 
 # Deleting container 
 #sudo docker rm -f $(sudo docker ps -aq)
