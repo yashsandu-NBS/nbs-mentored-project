b5c7a2b064fd48116c79a3daf7c296ed8ecd2368
yashsandu-nw <71331116+yashsandu-nw@users.noreply.github.com>
2020-11-04 10:37:31 +0000

    Update installation.sh

--- scripts/installation.sh
+++ scripts/installation.sh
@@ -1,6 +1,10 @@
 #! /bin/bash
 
-cd /home/student/nbs-mentored-project/
+sudo su - jenkins
+cd /home/student/nbs-mentored-project/scripts/packages/
+./install_python
+./install_ansible
 
 sudo apt install ansible -y
+exit
 ansible-playbook playbook.yaml
