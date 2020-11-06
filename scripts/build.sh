dc2452b55e82b1b519df4bf014c916e503115701
yashsandu-nw <71331116+yashsandu-nw@users.noreply.github.com>
2020-11-06 11:19:07 +0000

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,10 +1,3 @@
 #! /bin/bash
 
-# Full path to the requirements.txt
-
-# Installing the requirements.txt file
-#pip install -r requirements
-
-# Building the image
-# sudo docker build -t localhost:8082/service-1 .
 sudo docker-compose build
